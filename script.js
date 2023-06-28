7237913e3f2cf90a5c35e1c98c49f5bfde61b39b
ISADORA BERNARDES <88350340+isapbernardes@users.noreply.github.com>
2023-06-28 18:32:18 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,47 @@
+let movieNameRef = document.getElementById("movie-Name");
+let searchBtn = document.getElementById("search-Btn");
+let result = document.getElementById("result");
+
+let getMovie = () => {
+  let movieName = movieNameRef.value;
+  let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+
+  if (movieName.length <= 0) {
+    result.innerHTML = `<h3 class="msg">Please enter a movie name</h3>`;
+  } else {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (data.Response == "True") {
+          result.innerHTML = `<div class="info">
+            <img src="${data.Poster}" class="poster">
+            <div>
+              <h2>${data.Title}</h2>
+              <div class="rating">
+                <img src="estrela.png">
+                <h4>${data.imdbRating}</h4>
+              </div>
+              <div class="details">
+                <span>${data.Runtime}</span>
+              </div>
+              <div class="genre">
+                <div>${data.Genre.split(",").join("</div><div>")}</div>
+              </div>
+            </div>
+            <h3>Plot:</h3>
+            <p>${data.Plot}</p>
+            <h3>Cast:</h3>
+            <p>${data.Actors}</p>
+          </div>`;
+        } else {
+          result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+        }
+      })
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">Error Occurred</h3>`;
+      });
+  }
+};
+
+searchBtn.addEventListener("click", getMovie);
+window.addEventListener("load", getMovie);
